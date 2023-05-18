5437f2008d7f41e27fd743f0bbed538e10e53a6d
Jelte Dirks <j.dirks@outlook.com>
2023-05-18 20:38:57 +0200

    add index page and serve the index to every call
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 const ADDRESS: &str = "127.0.0.1:3001";
 
@@ -6,7 +9,30 @@ fn main() {
     let tcp_listener = TcpListener::bind(ADDRESS);
 
     for stream in tcp_listener.unwrap().incoming() {
-        let stream = stream.unwrap();
+        let mut stream = stream.unwrap();
         println!("connected to {ADDRESS}");
+
+        handle_connection(&mut stream);
+
+        let status_line = "HTTP/1.1 200 OK";
+        let content = std::fs::read_to_string("index.html").expect("index read");
+        let content_length = content.len();
+        let headers = format!("Content-Length: {content_length}\r\n");
+
+        let response = format!("{status_line}\r\n{headers}\r\n{content}");
+
+        stream.write_all(response.as_bytes()).expect("response msg");
     }
+}
+
+fn handle_connection(mut stream: &TcpStream) {
+    let reader = BufReader::new(&mut stream);
+    let http_req: Vec<_> = reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("incoming request:");
+    println!("{:#?}", http_req);
 }
